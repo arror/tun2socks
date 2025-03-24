77816b7b390c7b0266c708e8687bd3bbbbbcc047
Arror <hallo.maqiang@icloud.com>
2025-03-24 09:41:54 +0800

    移除不必要代码

--- proxy/proto/proto.go
+++ proxy/proto/proto.go
@@ -5,11 +5,7 @@ import "fmt"
 const (
 	Direct Proto = iota
 	Reject
-	HTTP
-	Socks4
 	Socks5
-	Shadowsocks
-	Relay
 )
 
 type Proto uint8
@@ -20,16 +16,8 @@ func (proto Proto) String() string {
 		return "direct"
 	case Reject:
 		return "reject"
-	case HTTP:
-		return "http"
-	case Socks4:
-		return "socks4"
 	case Socks5:
 		return "socks5"
-	case Shadowsocks:
-		return "ss"
-	case Relay:
-		return "relay"
 	default:
 		return fmt.Sprintf("proto(%d)", proto)
 	}
